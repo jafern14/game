fc491095625a28e5f4cf762a724f15c7fcdc22f4
Andy Weber <weber.andrew1@gmail.com>
2015-08-18 13:46:58 -0400

    Code Linting

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,10 @@
 window.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
 function preload () {
-
     game.load.image('logo', 'assets/textures/phaserimg.png');
-
 }
 
 function create () {
-
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
-
 }
--- client/src/output.js
+++ client/src/output.js
@@ -2,15 +2,11 @@
 window.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
 function preload () {
-
     game.load.image('logo', 'assets/textures/phaserimg.png');
-
 }
 
 function create () {
-
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5, 0.5);
-
 }
 },{}]},{},[1]);
