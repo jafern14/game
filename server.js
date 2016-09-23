b23ef4a5fe932653dece2e715a48ef2b5e7a01f0
Andy Weber <andrew.weber@opower.com>
2016-09-23 19:58:38 -0400

    express-webpack integration

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 var express = require('express');
-var http = require('http');
+var httpProxy = require('http-proxy');
 
 var app = express();
+var proxy = httpProxy.createProxyServer();
 
-app.use(express.static('app'));
-app.use('/scripts', express.static( __dirname + '/node_modules'));
+var port = isProduction ? process.env.PORT : 3000;
+var isProduction = process.env.NODE_ENV === 'production';
 
-var webServer = http.createServer(app).listen(8000);
+app.use(express.static('public'));
+app.use('/assets', express.static('assets'));
+
+// We only want to run the workflow when not in production
+if (!isProduction) {
+
+  // We require the bundler inside the if block because
+  // it is only needed in a development environment. Later
+  // you will see why this is a good idea
+  var bundle = require('./server/bundle.js');
+  bundle();
+
+  // Any requests to localhost:3000/build is proxied
+  // to webpack-dev-server
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    });
+  });
+
+}
+
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,36 @@
+var Webpack = require('webpack');
+var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var mainPath = path.resolve(__dirname, 'app', 'main.js');
+
 var config = {
-    entry: "./app/main.js",
+    devtool: "eval",
+    entry: [
+        // For hot style updates
+        'webpack/hot/dev-server',
+        // The script refreshing the browser on none hot updates
+        'webpack-dev-server/client?http://localhost:8080',
+        mainPath    
+    ],
     output: {
-        path: "./dist",
-        filename: "output.js"
-    }
+        path: buildPath,
+        filename: 'bundle.js',
+        publicPath: '/build/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test: /\.tmx$/,
+                loader: 'style!tmx'
+            }
+        ]
+    },
+    plugins: [new Webpack.HotModuleReplacementPlugin()]
 }
+
 module.exports = config;
